24ca84fd8d52315012c59c9c28a9f94b3aa6ed3b
Filip-Povidernyi <p.poviderniy@gmail.com>
2024-02-15 18:23:11 +0200

    hw

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,58 @@
+class Key {
+    private signature: number;
+    constructor() {
+        this.signature = Math.random();
+    };
+
+    getSignature(): number {
+        return this.signature;
+    };
+};
+
+class Person {
+    private key: Key;
+    constructor(key: Key) {
+        this.key;
+    };
+    getKey(): Key {
+        return this.key;
+    };
+};
+
+abstract class House {
+    protected door: true | false;
+    protected key: Key;
+    protected tenants: Person[] = [];
+    constructor(key: Key) {
+        this.door = false;
+        this.key = key;
+    };
+    abstract openDoor(key: Key): void;
+
+    comeIn(person: Person): void {
+        if (this.door) {
+            this.tenants.push(person);
+            console.log('Wellcome');
+        } else {
+            console.log('Door are close');
+        };
+    };
+};
+
+class MyHouse extends House {
+    openDoor(key: Key): void {
+        if (key.getSignature() === this.key.getSignature()) {
+            this.door = true;
+            console.log('Door are open');
+        } else {
+            console.log('You got a wrong key');
+        };
+    };
+};
+
+
+
+
 const key = new Key();
 
 const house = new MyHouse(key);
